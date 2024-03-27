93e4f7f276dcb0efa07b937cf9e12e3313e6947e
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-03-27 02:26:58 -0300

    Atualização de usuário com PUT

--- src/main/java/com/spring/workshopmongo/services/UserService.java
+++ src/main/java/com/spring/workshopmongo/services/UserService.java
@@ -30,13 +30,28 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-	}
-	
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
+	
+	public User update(User obj) {
+		Optional<User> newObj=repository.findById(obj.getId());
+		updateData(newObj,obj);
+		return repository.save(newObj.get());
+		
+	}
+	
+	private void updateData(Optional<User> newObj, User obj) {
+		newObj.get().setName(obj.getName());
+		newObj.get().setEmail(obj.getEmail());
+		
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 	
 }
