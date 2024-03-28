9ab078953f6da1a3323db3ea2651489c52fddb2b
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-03-28 01:28:04 -0300

    Obtendo um post por id

--- src/main/java/com/spring/workshopmongo/resources/UserResource.java
+++ src/main/java/com/spring/workshopmongo/resources/UserResource.java
@@ -68,10 +68,6 @@ public class UserResource {
 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
 		User obj=service.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
-		
-		
 	}
 	
-	
-
 }
